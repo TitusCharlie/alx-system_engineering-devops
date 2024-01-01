e22a63bd9d0662914b9e0cb716690a40a67911ea
TLC <tituslifecharlie@gmail.com>
2024-01-01 23:11:30 +0100

    corrected with embedded puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,22 +2,15 @@
 
 file { '/etc/ssh/ssh_config':
   ensure  => present,
-  content => epp('my_module/ssh_config.erb'),
-}
-
-file { '/home/freelance/.ssh/school':
-  ensure  => present,
-  source  => '/path/to/your/private/key',  # Update this path with the actual path to your private key
-  mode    => '0600',
-  owner   => 'freelance',
-  group   => 'freelance',
-}
-
-# Create a template for the SSH client configuration
-# Disable password authentication and specify the private key
-epp { '/etc/puppet/modules/my_module/templates/ssh_config.erb':
-  ensure  => present,
-  source  => 'puppet/ssh_config.erb',
+  content => "#!/usr/bin/env bash\n
+# SSH client configuration\n\n
+Host *\n
+        PasswordAuthentication no\n
+        IdentityFile ~/.ssh/school\n
+        SendEnv LANG LC_*\n
+        HashKnownHosts yes\n
+        GSSAPIAuthentication yes\n
+        GSSAPIDelegateCredentials no\n",
   mode    => '0644',
   owner   => 'root',
   group   => 'root',
