0860526d441797e49f9130888ae1a973c464711b
TLC <97347331+TitusCharlie@users.noreply.github.com>
2024-08-19 21:26:07 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+ function that queries the Reddit API and prints
+ the titles of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
+
 
 def top_ten(subreddit):
     # Define the base URL for the Reddit API with the subreddit and specify 'hot' posts
